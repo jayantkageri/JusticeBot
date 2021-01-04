9178fb2262229d29fbd81f8c5125f9c4c5a4725b
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-01-04 11:07:42 +0530

    Update misc.py

--- skylee/modules/misc.py
+++ skylee/modules/misc.py
@@ -29,8 +29,7 @@ from skylee import (
     SUDO_USERS,
     SUPPORT_USERS,
     WHITELIST_USERS,
-    WALL_API,
-    spamwtc,
+
 )
 from skylee.__main__ import STATS, USER_INFO, GDPR
 from skylee.modules.disable import DisableAbleCommandHandler
@@ -109,7 +108,7 @@ def info(update, context):
         return
 
     del_msg = msg.reply_text(
-        "Hold tight while I steal some data from <b>FBI Database</b>...",
+        "Getting Information...",
         parse_mode=ParseMode.HTML,
     )
 
@@ -131,35 +130,23 @@ def info(update, context):
         context.bot.get_user_profile_photos(user.id).total_count
     )
 
-    try:
-        sw = spamwtc.get_ban(int(user.id))
-        if sw:
-            text += "\n\n<b>This person is banned in Spamwatch!</b>"
-            text += f"\nResason: <pre>{sw.reason}</pre>"
-        else:
-            pass
-    except:
-        pass  # Don't break on exceptions like if api is down?
 
     if user.id == OWNER_ID:
-        text += "\n\nAye this guy is my owner.\nI would never do anything against him!"
+        text += "\n\nThis user is My Owner"
 
     elif user.id in SUDO_USERS:
         text += (
-            "\n\nThis person is one of my sudo users! "
-            "Nearly as powerful as my owner - so watch it."
+            "\n\nThis person is one of my sudo users! ""
         )
 
     elif user.id in SUPPORT_USERS:
         text += (
-            "\n\nThis person is one of my support users! "
-            "Not quite a sudo user, but can still gban you off the map."
+            "\n\nThis person is one of my support users! ""
         )
 
     elif user.id in WHITELIST_USERS:
         text += (
             "\n\nThis person has been whitelisted! "
-            "That means I'm not allowed to ban/kick them."
         )
 
     try:
@@ -208,23 +195,6 @@ def echo(update, context):
     message.delete()
 
 
-@run_async
-@typing_action
-def gdpr(update, context):
-    update.effective_message.reply_text("Deleting identifiable data...")
-    for mod in GDPR:
-        mod.__gdpr__(update.effective_user.id)
-
-    update.effective_message.reply_text(
-        "Your personal data has been deleted.\n\nNote that this will not unban "
-        "you from any chats, as that is telegram data, not Skylee data. "
-        "Flooding, warns, and gbans are also preserved, as of "
-        "[this](https://ico.org.uk/for-organisations/guide-to-the-general-data-protection-regulation-gdpr/individual-rights/right-to-erasure/), "
-        "which clearly states that the right to erasure does not apply "
-        '"for the performance of a task carried out in the public interest", as is '
-        "the case for the aforementioned pieces of data.",
-        parse_mode=ParseMode.MARKDOWN,
-    )
 
 
 MARKDOWN_HELP = """
@@ -255,143 +225,6 @@ Keep in mind that your message <b>MUST</b> contain some text other than just a b
     dispatcher.bot.first_name
 )
 
-
-@run_async
-@typing_action
-def markdown_help(update, context):
-    update.effective_message.reply_text(MARKDOWN_HELP, parse_mode=ParseMode.HTML)
-    update.effective_message.reply_text(
-        "Try forwarding the following message to me, and you'll see!"
-    )
-    update.effective_message.reply_text(
-        "/save test This is a markdown test. _italics_, --underline--, *bold*, `code`, ~strike~ "
-        "[URL](example.com) [button](buttonurl:github.com) "
-        "[button2](buttonurl://google.com:same)"
-    )
-
-
-@run_async
-@typing_action
-def wiki(update, context):
-    kueri = re.split(pattern="wiki", string=update.effective_message.text)
-    wikipedia.set_lang("en")
-    if len(str(kueri[1])) == 0:
-        update.effective_message.reply_text("Enter keywords!")
-    else:
-        try:
-            pertama = update.effective_message.reply_text("🔄 Loading...")
-            keyboard = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="🔧 More Info...", url=wikipedia.page(kueri).url
-                        )
-                    ]
-                ]
-            )
-            context.bot.editMessageText(
-                chat_id=update.effective_chat.id,
-                message_id=pertama.message_id,
-                text=wikipedia.summary(kueri, sentences=10),
-                reply_markup=keyboard,
-            )
-        except wikipedia.PageError as e:
-            update.effective_message.reply_text(f"⚠ Error: {e}")
-        except BadRequest as et:
-            update.effective_message.reply_text(f"⚠ Error: {et}")
-        except wikipedia.exceptions.DisambiguationError as eet:
-            update.effective_message.reply_text(
-                f"⚠ Error\n There are too many query! Express it more!\nPossible query result:\n{eet}"
-            )
-
-
-@run_async
-@typing_action
-def ud(update, context):
-    msg = update.effective_message
-    args = context.args
-    text = " ".join(args).lower()
-    if not text:
-        msg.reply_text("Please enter keywords to search!")
-        return
-    elif text == "starry":
-        msg.reply_text("Fek off bitch!")
-        return
-    try:
-        results = get(f"http://api.urbandictionary.com/v0/define?term={text}").json()
-        reply_text = f'Word: {text}\nDefinition: {results["list"][0]["definition"]}'
-        reply_text += f'\n\nExample: {results["list"][0]["example"]}'
-    except IndexError:
-        reply_text = (
-            f"Word: {text}\nResults: Sorry could not find any matching results!"
-        )
-    ignore_chars = "[]"
-    reply = reply_text
-    for chars in ignore_chars:
-        reply = reply.replace(chars, "")
-    if len(reply) >= 4096:
-        reply = reply[:4096]  # max msg lenth of tg.
-    try:
-        msg.reply_text(reply)
-    except BadRequest as err:
-        msg.reply_text(f"Error! {err.message}")
-
-
-@run_async
-@typing_action
-def src(update, context):
-    update.effective_message.reply_text(
-        "Hey there! You can find what makes me click [Here](t.me/meikoSupport) .",
-        parse_mode=ParseMode.MARKDOWN,
-        disable_web_page_preview=True,
-    )
-
-
-@run_async
-@send_action(ChatAction.UPLOAD_PHOTO)
-def wall(update, context):
-    chat_id = update.effective_chat.id
-    msg = update.effective_message
-    msg_id = update.effective_message.message_id
-    args = context.args
-    query = " ".join(args)
-    if not query:
-        msg.reply_text("Please enter a query!")
-        return
-    else:
-        caption = query
-        term = query.replace(" ", "%20")
-        json_rep = r.get(
-            f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}"
-        ).json()
-        if not json_rep.get("success"):
-            msg.reply_text("An error occurred!")
-
-        else:
-            wallpapers = json_rep.get("wallpapers")
-            if not wallpapers:
-                msg.reply_text("No results found! Refine your search.")
-                return
-            else:
-                index = randint(0, len(wallpapers) - 1)  # Choose random index
-                wallpaper = wallpapers[index]
-                wallpaper = wallpaper.get("url_image")
-                wallpaper = wallpaper.replace("\\", "")
-                context.bot.send_photo(
-                    chat_id,
-                    photo=wallpaper,
-                    caption="Preview",
-                    reply_to_message_id=msg_id,
-                    timeout=60,
-                )
-                context.bot.send_document(
-                    chat_id,
-                    document=wallpaper,
-                    filename="wallpaper",
-                    caption=caption,
-                    reply_to_message_id=msg_id,
-                    timeout=60,
-                )
 
 
 @run_async
@@ -424,58 +257,6 @@ def getlink(update, context):
 
 
 @run_async
-@send_action(ChatAction.UPLOAD_PHOTO)
-def rmemes(update, context):
-    msg = update.effective_message
-    chat = update.effective_chat
-
-    SUBREDS = [
-        "meirl",
-        "dankmemes",
-        "AdviceAnimals",
-        "memes",
-        "meme",
-        "memes_of_the_dank",
-        "PornhubComments",
-        "teenagers",
-        "memesIRL",
-        "insanepeoplefacebook",
-        "terriblefacebookmemes",
-    ]
-
-    subreddit = random.choice(SUBREDS)
-    res = r.get(f"https://meme-api.herokuapp.com/gimme/{subreddit}")
-
-    if res.status_code != 200:  # Like if api is down?
-        msg.reply_text("Sorry some error occurred :(")
-        return
-    else:
-        res = res.json()
-
-    rpage = res.get(str("subreddit"))  # Subreddit
-    title = res.get(str("title"))  # Post title
-    memeu = res.get(str("url"))  # meme pic url
-    plink = res.get(str("postLink"))
-
-    caps = f"✗ <b>Title</b>: {title}\n"
-    caps += f"✗ <b>Subreddit:</b> <pre>r/{rpage}</pre>"
-
-    keyb = [[InlineKeyboardButton(text="Subreddit Postlink 🔗", url=plink)]]
-    try:
-        context.bot.send_photo(
-            chat.id,
-            photo=memeu,
-            caption=(caps),
-            reply_markup=InlineKeyboardMarkup(keyb),
-            timeout=60,
-            parse_mode=ParseMode.HTML,
-        )
-
-    except BadRequest as excp:
-        return msg.reply_text(f"Error! {excp.message}")
-
-
-@run_async
 def staff_ids(update, context):
     sfile = "List of SUDO & SUPPORT users:\n"
     sfile += f"✗ SUDO USER IDs; {SUDO_USERS}\n"
@@ -502,12 +283,6 @@ An "odds and ends" module for small, simple commands which don't really fit anyw
 
  ✗ /id: Get the current group id. If used by replying to a message, gets that user's id.
  ✗ /info: Get information about a user.
- ✗ /wiki : Search wikipedia articles.
- ✗ /rmeme: Sends random meme scraped from reddit.
- ✗ /ud <query> : Search stuffs in urban dictionary.
- ✗ /wall <query> : Get random wallpapers directly from bot! 
- ✗ /reverse : Reverse searches image or stickers on google.
- ✗ /gdpr: Deletes your information from the bot's database. Private chats only.
  ✗ /markdownhelp: Quick summary of how markdown works in telegram - can only be called in private chats.
 """
 
@@ -518,29 +293,18 @@ INFO_HANDLER = DisableAbleCommandHandler("info", info, pass_args=True)
 ECHO_HANDLER = CommandHandler("echo", echo, filters=CustomFilters.sudo_filter)
 MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help, filters=Filters.private)
 STATS_HANDLER = CommandHandler("stats", stats, filters=Filters.user(OWNER_ID))
-GDPR_HANDLER = CommandHandler("gdpr", gdpr, filters=Filters.private)
-WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
-WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall, pass_args=True)
-UD_HANDLER = DisableAbleCommandHandler("ud", ud)
 GETLINK_HANDLER = CommandHandler(
     "getlink", getlink, pass_args=True, filters=Filters.user(OWNER_ID)
 )
 STAFFLIST_HANDLER = CommandHandler(
     "staffids", staff_ids, filters=Filters.user(OWNER_ID)
 )
-REDDIT_MEMES_HANDLER = DisableAbleCommandHandler("rmeme", rmemes)
-SRC_HANDLER = CommandHandler("source", src, filters=Filters.private)
 
-dispatcher.add_handler(WALLPAPER_HANDLER)
-dispatcher.add_handler(UD_HANDLER)
 dispatcher.add_handler(ID_HANDLER)
 dispatcher.add_handler(INFO_HANDLER)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 dispatcher.add_handler(STATS_HANDLER)
-dispatcher.add_handler(GDPR_HANDLER)
-dispatcher.add_handler(WIKI_HANDLER)
 dispatcher.add_handler(GETLINK_HANDLER)
 dispatcher.add_handler(STAFFLIST_HANDLER)
-dispatcher.add_handler(REDDIT_MEMES_HANDLER)
-dispatcher.add_handler(SRC_HANDLER)
+
