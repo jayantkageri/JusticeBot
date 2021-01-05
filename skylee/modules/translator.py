ac8ced5da80883760d96a76378b955e8d93a8704
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-01-05 14:46:54 +0530

    Update translator.py

--- skylee/modules/translator.py
+++ skylee/modules/translator.py
@@ -55,12 +55,12 @@ def gtts(update, context):
             reply = reply.replace(x, "")
     try:
         tts = gTTS(reply)
-        tts.save("meikobot.mp3")
-        with open("meikobot.mp3", "rb") as speech:
+        tts.save("jayantkageri.mp3")
+        with open("jayantkageri.mp3", "rb") as speech:
             msg.reply_audio(speech)
     finally:
-        if os.path.isfile("meikobot.mp3"):
-            os.remove("meikobot.mp3")
+        if os.path.isfile("jayantkageri.mp3"):
+            os.remove("jayantkageri.mp3")
 
 
 # Open API key
